8c19349462495f4b1b6facbbfa7712d532b1b727
sara rasoulian <sara.rasoulian.dev@gmail.com>
2024-07-07 20:51:18 +0330

    Implement response caching

---
+++ SampleApp/SampleApp/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SampleApp.Data;
+
+namespace SampleApp.Controllers;
+
+[Route("api/products")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private readonly SampleDBContext _dbContext;
+    public ProductsController(SampleDBContext dBContext)
+    {
+        _dbContext = dBContext;
+    }
+
+    [HttpGet("get-with-response-cache")]
+    [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any, NoStore = false)]
+    public async Task<IActionResult> GetWithResponseCache()
+    {
+        var result = await _dbContext.Products.ToListAsync();
+        return Ok(result);
+    }
+}
